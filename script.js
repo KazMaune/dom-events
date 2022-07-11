1d1c9719a3186f88725e1fdf01523791a40093bd
Kaz <103293670+ZerxiVT@users.noreply.github.com>
2022-07-11 21:17:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function init () {
+    const missionAbort = document.getElementById("abortMission");
+    const button = document.getElementById("liftoffButton");
+    const paragraph = document.getElementById("statusReport");
+
+    // Put your code for the exercises here.
+    button.addEventListener("click", function(){
+      paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+    missionAbort.addEventListener("mouseenter", function() {
+      missionAbort.style.backgroundColor = "red";
+    }); 
+    missionAbort.addEventListener("mouseleave", function(){
+      missionAbort.style.backgroundColor = "";
+    });
+    missionAbort.addEventListener("click", function() {
+      if (window.confirm("Are you sure you want to abort the mission?")) {
+        paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+      }    
+    });
+}
+
+window.addEventListener("load", init);
